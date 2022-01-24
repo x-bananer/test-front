9bb2de25eb7a87a90a232471f3ebc09d56206b9a
Ksenia Shlyonskaya <xeniya.shlyonskaya@gmail.com>
2022-01-24 12:15:42 +0300

    deploy attempt

---
+++ docs/_nuxt/static/1643015332/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-front\u002F_nuxt\u002Fstatic\u002F1643015332",layout:"default",error:a,state:{companies:{companies:a,definitions:a,company:a}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftest-front\u002F",assetsPath:"\u002Ftest-front\u002F_nuxt\u002F",cdnURL:a}}}}(null));
